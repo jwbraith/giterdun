64710ec0de6b41b6558afb847e6bc55a65c658b9
jwbraith <jwbraith@gmail.com>
2019-07-21 00:23:55 -0700

    updating the terminal commands I'm trying to learn

--- giturden.js
+++ giturden.js
@@ -15,3 +15,11 @@ function setup() {
 
 //Node commands
 //http-server
+
+//Git commands
+//git clone			--		pulls a directory from an address ie github
+//git status		--		see the current such-and-such of the directory
+//git log       --		who's made changes
+//git commit -a -m "comment"		-- commit all changes in the staging area
+//git remote		--		list of the far flung corresponding directory
+//git add				--		add a specific or all (.) files to the staging area
