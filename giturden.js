7fe11f0e4e9edc654b5c3390e44585e16277b931
jwbraith <jwbraith@gmail.com>
2019-07-21 00:15:54 -0700

    adding a js file

---
+++ giturden.js
@@ -0,0 +1,17 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+// actually just going to write a bunch of commands i want to learn
+//Terminal commands
+//cd		--		Change Directory
+//pwd		--		Print Working Directory
+//ls		--		List
+
+
+//p5 manager commands
+//P5 generate --bundle "foldername"
+
+
+//Node commands
+//http-server
